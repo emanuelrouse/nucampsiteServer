8c6f38e05d92ad754862fb1850583b451562e911
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-02-06 21:54:45 -0500

    Basic Authentication

--- app.js
+++ app.js
@@ -24,6 +24,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+	console.log(req.headers);
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		const err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		// standard error statusCode when the user isn't authenticated is 401
+		err.status = 401;
+		return next(err);
+	}
+
+	const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+	const user = auth[0];
+	const pass = auth[1];
+
+	// basic validation
+	if (user === 'admin' && pass === 'password') {
+		return next(); // authorized
+	} else {
+		// Unauthorized User
+		const err = new Error('You are not authenticated!');
+		// set header to basic authentication
+		res.setHeader('WWW-Authenticate', 'Basic');
+		// set status property on error obj to 401
+		err.status = 401;
+		// send off to next middleware
+		next(err);
+	}
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
@@ -38,31 +70,31 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+	useCreateIndex: true,
+	useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
 // Another way to handle errors other than catch method is to pass the err a second argument to the .then
 connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
+	err => console.log(err)
 );
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
